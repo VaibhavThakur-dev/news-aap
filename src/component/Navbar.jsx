787f3f32527b407e25a100738efd96e1a378fc48
VaibhavThakur-dev <152158096+VaibhavThakur-dev@users.noreply.github.com>
2024-01-24 01:22:30 +0530

    commit

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+      <h1 className='text-xs sm:text-base md:text-lg lg:text-5xl' >this is a test </h1>
+    </>
+  )
+}
+
+export default Navbar
